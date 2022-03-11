77ac6554e2f162cf171ce4e41a297bb010797278
Achille kabasele <pepexykabasele@gmail.com>
2022-03-11 17:23:56 +0200

    first commit variables

--- variable/main.dart
+++ variable/main.dart
@@ -1,10 +1,25 @@
 // Declarations de Variables
 
 var x = 5;
+
+// Dans Dart, ce code attribue la valeur 5 à la variable x. De même, l'exemple suivant affecte le littéral de chaîne
+// "antilope" aux variables animal.
+
 var animal = "antelope";
 
+// Dans l'exemple suivant, la variable mycoolWord reçoit la valeur de la chaîne littérale "antilope". Les variables peuvent
+// ont aussi un type. Pour l'instant, vous pouvez considérer un type comme un moyen d'insister sur le fait qu'une variable ne contient qu'un type spécifique de valeur,
+// même si ce n'est pas tout à fait exact. mycoolWord est spécifié comme étant de type String
+
+String mycoolWord = "other antelope";
 // Programme principal
 void main(List<String> args) {
+  // rar x
   print(x);
+  // var animal
   print(animal);
+
+  // mycoolWord
+
+  print(mycoolWord);
 }
